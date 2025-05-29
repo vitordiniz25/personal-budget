72a86a15ad84c161ed9ed1a856566e1a2024d494
Vitor Diniz <vitordiniz25@gmail.com>
2025-05-29 16:20:55 -0300

    chore: initial api setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome to the API!");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
