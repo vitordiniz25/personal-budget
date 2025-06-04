cd96a32fdc24cbb2c1d235d2d79bd1a7ae83cdb3
Vitor Diniz <vitordiniz25@gmail.com>
2025-06-04 12:05:57 -0300

    docs: update base url for swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,8 +11,8 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "http://localhost:3000",
-      description: "Servidor local",
+      url: "/",
+      description: "Base URL",
     },
   ],
   components,
